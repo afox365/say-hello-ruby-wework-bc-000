0635cf92cfba873a8b297b732b52401ab86c7714
afox@wework.com <afox@wework.com>
2018-02-16 02:06:18 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,11 +1,7 @@
 # Build your say_hello method here
 
-def say_hello(name)
-  puts "Hello #{name}!"
-end
-
-def say_hello
-  puts "Hello Ruby Programmer!"
+def say_hello(name="Ruby programmer")
+  puts "Hello, #{name}!"
 end
 
 say_hello("Kent Beck")
